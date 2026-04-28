8b287878b3f6fbdbbc353bfa891d0e1028b7a744
Krzysztof <klachowskia@gmail.com>
2026-04-28 23:47:07 +0200

    Konfiguracja projektu, bazy danych i swaggera

--- ClinicApi/obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
+++ ClinicApi/obj/Debug/net8.0/ClinicApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c695bb84e0dce3ef37899b30b682d5577ba2ad9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
